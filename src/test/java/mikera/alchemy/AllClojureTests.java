beb52c9c0b6bf19717c143dbb4d2335971d29c32
mikera <mike.r.anderson.13@gmail.com>
2013-03-10 18:47:09 +0800

    add cljunit testing

---
+++ src/test/java/mikera/alchemy/AllClojureTests.java
@@ -0,0 +1,11 @@
+package mikera.alchemy;
+
+import mikera.cljunit.ClojureTest;
+
+public class AllClojureTests extends ClojureTest {
+
+	public String filter() {
+		return "mikera.alchemy";
+	}
+
+}
