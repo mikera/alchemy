8fb8fac7517d1af81084d0aa4745e4dda3622e44
mikera <mike.r.anderson.13@gmail.com>
2013-03-11 16:24:35 +0800

    Basic draw functions

--- src/main/clojure/mikera/alchemy/main.clj
+++ src/main/clojure/mikera/alchemy/main.clj
@@ -1,6 +1,7 @@
 (ns mikera.alchemy.main
+  (:require [mikera.orculje.gui :as gui])
   (:import [javax.swing JFrame JComponent])
-  (:import [java.awt Font])
+  (:import [java.awt Font Color])
   (:import [mikera.gui JConsole]))
 
 (set! *warn-on-reflection* true)
@@ -22,6 +23,18 @@
       (.setCursorBlink jc false)
       jc)))
 
+(defn redraw-screen [state]
+  (let [^JConsole jc (:console state)
+        w (.getColumns jc)
+        h (.getRows jc)
+        gw (- w 20)
+        gh (- h 5)]
+    (.setBackground jc (Color. 0x203040))
+    (dotimes [y gh]
+      (dotimes [x gw]
+        (gui/draw jc x y (str x))))
+    (.repaint jc)))
+
 (defn launch 
   "Launch the game with an initial game state. Can be called from REPL."
   ([state]
@@ -31,6 +44,7 @@
       (.pack frame)
       (.setVisible frame true)
       (.write jc "Alchemy Lives!") 
+      (redraw-screen state) 
       frame)))
 
 (defn new-state
