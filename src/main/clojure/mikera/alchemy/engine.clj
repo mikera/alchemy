6ca186e0b74fea7cea86ec88811e656415e8edda
mikera <mike.r.anderson.13@gmail.com>
2013-03-12 17:35:22 +0800

    bump and attach functions

--- src/main/clojure/mikera/alchemy/engine.clj
+++ src/main/clojure/mikera/alchemy/engine.clj
@@ -1,6 +1,7 @@
 (ns mikera.alchemy.engine
   (:require [mikera.cljutils.find :as find]) 
   (:use mikera.orculje.core)
+  (:use mikera.cljutils.error)
   (:require[ mikera.orculje.text :as text]))
 
 ;; message logging
@@ -17,7 +18,8 @@
 (defn message
   "Send a message to a given thing. Should be displaiyed iff it is the hero."
   ([game thing & ss]
-    (let [msgs (or (:messages game) [])
+    (let [ss (map text/capitalise ss)
+          msgs (or (:messages game) [])
           new-msgs (vec (concat msgs ss))
           mlog (or (:message-log game) [msgs])
           new-mlog (assoc mlog 0 new-msgs)]
@@ -34,8 +36,20 @@
         (find/find-first :is-blocking ts)
         (if (:is-blocking tl) tl nil))))) 
 
+(defn try-attack [game thing target]
+  (message game thing (str (text/verb-phrase :the thing "attack" :the target))))
+
+(defn try-bump [game thing target]
+  (cond
+    (:is-tile target)
+      (message game thing (str (text/verb-phrase :the thing "run") " into a wall."))
+    (:is-hostile target)
+      (try-attack game thing target)
+    :else
+      (error "Don't know hot to touch: " target)))
+
 (defn try-move
   [game thing loc]
-  (if (get-blocking game loc)
-    (message game thing (str (text/verb-phrase :the thing "run") " into a wall"))
+  (if-let [target (get-blocking game loc)]
+    (try-bump game thing target)
     (move-thing game thing loc)))
